9dd1f4e184ea2304e7dd3496bb5f453280f97117
zyspec <zyspec@yahoo.com>
2019-05-14 12:43:04 -0500

    Include missing files (search, issues, import)
    Include files inadvertently omitted in previous commit. Update
    ./docs/changelog.txt

---
+++ language/english/blocks.php
@@ -0,0 +1,7 @@
+<?php
+define('_MB_XFORMS_LIST_BLOCK_SORT_OPTS', 'Weight (Order),Title');
+define('_MB_XFORMS_SORTBY', 'Sort forms by:');
+define('_MB_XFORMS_NUM_FORMS', 'Number of forms to show:');
+
+define('_MB_XFORMS_FORM_DISPLAY', 'Form to Display');
+define('_MB_XFORMS_FORM_NONE', 'No Active Forms');
