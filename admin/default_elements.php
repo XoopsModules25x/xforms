a1176735e80c7a3bbe1f7a8f8f3ef9024fd6730d
zyspec <zyspec@yahoo.com>
2020-03-26 13:07:45 -0500

    Use short array syntax nomenclature
    - minor phpDoc cleanup

--- admin/default_elements.php
+++ admin/default_elements.php
@@ -11,51 +11,63 @@
  */
 /**
  * Module: xForms
- * Default settings for elements
+ *
+ * Settings for default form elements
  *
  * @package   \XoopsModules\Xforms\admin
  * @author    XOOPS Module Development Team
- * @copyright Copyright (c) 2001-2020 {@link http://xoops.org XOOPS Project}
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @copyright Copyright (c) 2001-2020 {@link https://xoops.org XOOPS Project}
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
  * @link      https://github.com/XoopsModules25x/xforms
  *
  * @see \XoopsModules\Xforms\Helper
  */
-use XoopsModules\Xforms;
+
 use XoopsModules\Xforms\Helper;
 
 defined('XOOPS_ROOT_PATH') || exit('Restricted access');
 
 // Instantiate classes
-/* @var \XoopsModules\Xforms\Helper $helper */
+/** @var \XoopsModules\Xforms\Helper $helper */
 $helper   = Helper::getInstance();     // module helper
-$defaults = array(0 => array('caption' => _AM_XFORMS_DEFAULT_ELE_YOURNAME,
-                                 'req' => true,
-                     'ele_display_row' => 1,
-                               'order' => 1,
-                             'display' => 1,
-                                'type' => 'text',
-                               'value' => array(0 => $helper->getConfig('t_width'),
-                                                1 => $helper->getConfig('t_max'),
-                                                2 => '{U_uname}')),
-
-                  1 => array('caption' => _AM_XFORMS_DEFAULT_ELE_YOUREMAIL,
-                                 'req' => true,
-                     'ele_display_row' => 1,
-                               'order' => 2,
-                             'display' => 1,
-                                'type' => 'email',
-                               'value' => array(0 => $helper->getConfig('t_width'),
-                                                1 => 254,
-                                                2 => '{U_email}')),
-
-                  2 => array('caption' => _AM_XFORMS_DEFAULT_ELE_COMMENTS,
-                                 'req' => true,
-                     'ele_display_row' => 1,
-                               'order' => 3,
-                             'display' => 1,
-                                'type' => 'textarea',
-                               'value' => array(0 => '',
-                                                1 => $helper->getConfig('ta_rows'),
-                                                2 => $helper->getConfig('ta_cols')))
-                  );
+$defaults = [
+    0 => [
+        'caption' => _AM_XFORMS_DEFAULT_ELE_YOURNAME,
+        'req'     => true,
+        'ele_display_row' => 1,
+        'order' => 1,
+        'display' => 1,
+        'type' => 'text',
+        'value' => [
+            0 => $helper->getConfig('t_width'),
+            1 => $helper->getConfig('t_max'),
+            2 => '{U_uname}'
+        ]
+    ],
+    1 => [
+        'caption' => _AM_XFORMS_DEFAULT_ELE_YOUREMAIL,
+        'req' => true,
+        'ele_display_row' => 1,
+        'order' => 2,
+        'display' => 1,
+        'type' => 'email',
+        'value' => [
+            0 => $helper->getConfig('t_width'),
+            1 => 254,
+            2 => '{U_email}'
+        ],
+    ],
+    2 => [
+        'caption' => _AM_XFORMS_DEFAULT_ELE_COMMENTS,
+        'req'             => true,
+        'ele_display_row' => 1,
+        'order'           => 3,
+        'display'         => 1,
+        'type'            => 'textarea',
+        'value'           => [
+            0 => '',
+            1 => $helper->getConfig('ta_rows'),
+            2 => $helper->getConfig('ta_cols')
+        ],
+    ],
+];
