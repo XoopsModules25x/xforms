aebae340e9f62bc82cf2d9cce6881dc182375aed
zyspec <zyspec@yahoo.com>
2020-03-27 15:24:05 -0500

    Small refactor of .admin/index.php to reduce dB accesses

--- admin/index.php
+++ admin/index.php
@@ -14,34 +14,39 @@
  *
  * @package   \XoopsModules\Xforms\admin
  * @author    XOOPS Module Development Team
- * @copyright Copyright (c) 2001-2019 {@link https://xoops.org XOOPS Project}
+ * @copyright Copyright (c) 2001-2020 {@link https://xoops.org XOOPS Project}
  * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @link      https://github.com/XoopsModules25x/xforms
  * @since     1.30
  *
- * @see \Xmf\Module\Helper
- * @see \Xmf\Module\Admin
  */
 use XoopsModules\Xforms;
 use XoopsModules\Xforms\Constants;
 
 require_once __DIR__ . '/admin_header.php';
 
+/**
+ * @var \Xmf\Module\Admin $adminObject
+ * @var \XoopsModules\Xforms\Helper $helper
+ * @var \XoopsModules\Xforms\FormsHandler $formsHandler
+ *
+ * @var string $moduleDirName
+ */
+
 xoops_cp_header();
-/* @var \XoopsModules\Xforms\FormsHandler $formsHandler */
-$totalForms         = $formsHandler->getCount();
-$criteria           = new \CriteriaCompo();
-$criteria->add(new \Criteria('form_active', Constants::FORM_ACTIVE, '='));
-$totalActiveForms   = $formsHandler->getCount($criteria);
+$criteria = new \Criteria('');
+$criteria->setGroupBy('form_active');
+$formCount          = $formsHandler->getCount($criteria);
+$totalForms         = array_sum($formCount);
+$totalActiveForms   = isset($formCount[1]) ? $formCount[1] : 0;
 $totalInactiveForms = $totalForms - $totalActiveForms;
 
-/* @var \Xmf\Module\Admin $adminObject */
 $adminObject->addInfoBox(_MD_XFORMS_DASHBOARD);
 $adminObject->AddInfoBoxLine(sprintf('<span class="infolabel">' . _MD_XFORMS_TOTAL_ACTIVE . '</span>', '<span class="infotext green bold">' . $totalActiveForms . '</span>'));
 $adminObject->addInfoBoxLine(sprintf('<span class="infolabel">' . _MD_XFORMS_TOTAL_INACTIVE . '</span>', '<span class="infotext red bold">' . $totalInactiveForms . '</span>'));
 $adminObject->addInfoBoxLine(sprintf('<span class="infolabel">' . _MD_XFORMS_TOTAL_FORMS . '</span>', '<span class="infotext bold">' . $totalForms . '</span>'));
 
 // Check for profile module
-/* @var Xmf\Module\Helper $profileHelper */
 $profileHelper = \Xmf\Module\Helper::getHelper('profile');
 if (false === $profileHelper) {
     $adminObject->addConfigWarning(sprintf(_MD_XFORMS_PROFILE_NOT_FOUND, $moduleDirName));
@@ -49,7 +54,7 @@ if (false === $profileHelper) {
     $adminObject->addConfigAccept(sprintf(_MD_XFORMS_PROFILE_FOUND, $moduleDirName));
 }
 
-/* @var \XoopsModules\Xforms\Utility $utility */
+/** @var \XoopsModules\Xforms\Utility $utility */
 $utility = new \XoopsModules\Xforms\Utility();
 
 //check for latest release
