b0c8a297e6f7da56747cc73e3a21f7748f671c35
mambax7 <mambax7@gmail.com>
2021-02-14 14:42:23 -0500

    PSR-12 Missing visibility

--- class/Constants.php
+++ class/Constants.php
@@ -31,283 +31,283 @@ interface Constants
      * Constant definition
      */
 
-    const DISALLOW = 0;
+    public const DISALLOW = 0;
     // CONFIG displayicons
-    const DISPLAYICONS_ICON = 1;
-    const DISPLAYICONS_TEXT = 2;
-    const DISPLAYICONS_NO = 3;
+    public const DISPLAYICONS_ICON = 1;
+    public const DISPLAYICONS_TEXT = 2;
+    public const DISPLAYICONS_NO = 3;
     // CONFIG submissions
-    const SUBMISSIONS_NONE = 1;
-    const SUBMISSIONS_DOWNLOAD = 2;
-    const SUBMISSIONS_MIRROR = 3;
-    const SUBMISSIONS_BOTH = 4;
+    public const SUBMISSIONS_NONE     = 1;
+    public const SUBMISSIONS_DOWNLOAD = 2;
+    public const SUBMISSIONS_MIRROR = 3;
+    public const SUBMISSIONS_BOTH = 4;
     // CONFIG anonpost
-    const ANONPOST_NONE = 1;
-    const ANONPOST_DOWNLOAD = 2;
-    const ANONPOST_MIRROR = 3;
-    const ANONPOST_BOTH = 4;
+    public const ANONPOST_NONE     = 1;
+    public const ANONPOST_DOWNLOAD = 2;
+    public const ANONPOST_MIRROR = 3;
+    public const ANONPOST_BOTH = 4;
     // CONFIG autoapprove
-    const AUTOAPPROVE_NONE = 1;
-    const AUTOAPPROVE_DOWNLOAD = 2;
-    const AUTOAPPROVE_MIRROR = 3;
-    const AUTOAPPROVE_BOTH = 4;
-    const DEFAULT_ELEMENT_SIZE = 1;
+    public const AUTOAPPROVE_NONE     = 1;
+    public const AUTOAPPROVE_DOWNLOAD = 2;
+    public const AUTOAPPROVE_MIRROR = 3;
+    public const AUTOAPPROVE_BOTH = 4;
+    public const DEFAULT_ELEMENT_SIZE = 1;
     /**
      * List Block Sort Keys - must be columns in the dB
      * must match the number of entities in define('_MB_XFORMS_LIST_BLOCK_SORT_OPTS
      */
-    const LIST_BLOCK_SORT_KEYS = 'form_order,form_title';
+    public const LIST_BLOCK_SORT_KEYS = 'form_order,form_title';
     /**
      * default forms to show per page in lists
      */
-    const FORMS_PER_PAGE_DEFAULT = 10;
+    public const FORMS_PER_PAGE_DEFAULT = 10;
     /**
      * form - invalid ID
      */
-    const FORM_NOT_VALID = 0;
+    public const FORM_NOT_VALID = 0;
     /**
      * form Active
      */
-    const FORM_ACTIVE = 1;
+    public const FORM_ACTIVE = 1;
     /**
      * form Inactive
      */
-    const FORM_INACTIVE = 0;
+    public const FORM_INACTIVE = 0;
     /**
      * Allow Multi select
      */
-    const ALLOW_MULTI = 1;
+    public const ALLOW_MULTI = 1;
     /**
      * Disallow Multi select
      */
-    const DISALLOW_MULTI = 0;
+    public const DISALLOW_MULTI = 0;
     /**
      * Allow HTML
      */
-    const ALLOW_HTML = 1;
+    public const ALLOW_HTML = 1;
     /**
      * Disallow HTML
      */
-    const DISALLOW_HTML = 0;
+    public const DISALLOW_HTML = 0;
     /**
      * Element - invalid ID
      */
-    const ELE_NOT_VALID = 0;
+    public const ELE_NOT_VALID = 0;
     /**
      * Element - no
      */
-    const ELE_NO = 0;
+    public const ELE_NO = 0;
     /**
      * Element - yes
      */
-    const ELE_YES = 1;
+    public const ELE_YES = 1;
     /**
      * element - scheme both http & ftp
      */
-    const SCHEME_BOTH = 0;
+    public const SCHEME_BOTH = 0;
     /**
      * element - scheme both http & ftp
      */
-    const SCHEME_HTTP = 1;
+    public const SCHEME_HTTP = 1;
     /**
      * element - scheme both http & ftp
      */
-    const SCHEME_FTP = 2;
+    public const SCHEME_FTP = 2;
     /**
      * element - use current date
      */
-    const ELE_CURR = 1;
+    public const ELE_CURR = 1;
     /**
      * element - use other date
      */
-    const ELE_OTHER = 2;
+    public const ELE_OTHER = 2;
     /**
      * element - set date
      */
-    const DATE_NONE = 0;
+    public const DATE_NONE = 0;
     /**
      * element - set to current date
      */
-    const DATE_CURRENT = 1;
+    public const DATE_CURRENT = 1;
     /**
      * element - set specific date
      */
-    const DATE_SPECIFIC = 2;
+    public const DATE_SPECIFIC = 2;
     /**
      * range default step size
      */
-    const ELE_DEFAULT_STEP = 1;
+    public const ELE_DEFAULT_STEP = 1;
     /**
      * Element not checked
      */
-    const ELE_NOT_CHECKED = 0;
+    public const ELE_NOT_CHECKED = 0;
     /**
      * Element checked
      */
-    const ELE_CHECKED = 1;
+    public const ELE_CHECKED = 1;
     /**
      * form save in database
      */
-    const SAVE_IN_DB = 1;
+    public const SAVE_IN_DB = 1;
     /**
      * form do not save in database
      */
-    const DO_NOT_SAVE_IN_DB = 0;
+    public const DO_NOT_SAVE_IN_DB = 0;
     /**
      * hidden form
      */
-    const FORM_HIDDEN = 0;
+    public const FORM_HIDDEN = 0;
     /**
      * show the form list
      */
-    const FORM_LIST_SHOW = 1;
+    public const FORM_LIST_SHOW = 1;
     /**
      * hide the form list
      */
-    const FORM_LIST_NO_SHOW = 0;
+    public const FORM_LIST_NO_SHOW = 0;
     /**
      * form display style - form (f)
      */
-    const FORM_DISPLAY_STYLE_FORM = 'f';
+    public const FORM_DISPLAY_STYLE_FORM = 'f';
     /**
      * form display style - poll (p)
      */
-    const FORM_DISPLAY_STYLE_POLL = 'p';
+    public const FORM_DISPLAY_STYLE_POLL = 'p';
     /**
      * Required (form element setting)
      */
-    const REQUIRED = 1;
+    public const REQUIRED = 1;
     /**
      * indicates the form element is NOT required
      */
-    const ELEMENT_NOT_REQD = 0;
+    public const ELEMENT_NOT_REQD = 0;
     /**
      * indicates the form element is required
      */
-    const ELEMENT_REQD = 1;
+    public const ELEMENT_REQD = 1;
     /**
      * display element
      */
-    const ELEMENT_DISPLAY = 1;
+    public const ELEMENT_DISPLAY = 1;
     /**
      * do not display element
      */
-    const ELEMENT_NOT_DISPLAY = 0;
+    public const ELEMENT_NOT_DISPLAY = 0;
     /**
      * display element on two rows in form
      */
-    const DISPLAY_DOUBLE_ROW = 2;
+    public const DISPLAY_DOUBLE_ROW = 2;
     /**
      * display element on one row in form
      */
-    const DISPLAY_SINGLE_ROW = 1;
+    public const DISPLAY_SINGLE_ROW = 1;
     /**
      * save uploaded file to mail
      */
-    const FILE_AS_MAIL = 0;
+    public const FILE_AS_MAIL = 0;
     /**
      * save uploaded file to file
      */
-    const FILE_AS_FILE = 1;
+    public const FILE_AS_FILE = 1;
     /**
      * form text field is not an email
      */
-    const FIELD_IS_NOT_EMAIL = 0;
+    public const FIELD_IS_NOT_EMAIL = 0;
     /**
      * form text field is an email addr
      */
-    const FIELD_IS_EMAIL = 1;
+    public const FIELD_IS_EMAIL = 1;
     /**
      * indicates form results sent via email
      */
-    const SEND_METHOD_MAIL = 'e';
+    public const SEND_METHOD_MAIL = 'e';
     /**
      * indicates form results sent via private message
      */
-    const SEND_METHOD_PM = 'p';
+    public const SEND_METHOD_PM = 'p';
     /**
      * indicates form results will not be sent (none)
      */
-    const SEND_METHOD_NONE = 'n';
+    public const SEND_METHOD_NONE = 'n';
     /**
      * indicates a copy of form results will not be sent
      */
-    const SEND_NO_COPY = 0;
+    public const SEND_NO_COPY = 0;
     /**
      * indicates a copy of form results will be sent
      */
-    const SEND_COPY = 1;
+    public const SEND_COPY = 1;
     /**
      * send copy to others
      */
-    const SEND_TO_OTHER = -1;
+    public const SEND_TO_OTHER = -1;
     /**
      * don't send copy
      */
-    const SEND_TO_NONE = 0;
+    public const SEND_TO_NONE = 0;
     /**
      * save uploaded file as mail
      */
-    const UPLOAD_SAVEAS_ATTACHMENT = 0;
+    public const UPLOAD_SAVEAS_ATTACHMENT = 0;
     /**
      * save uploaded file as attachment
      */
-    const UPLOAD_SAVEAS_FILE = 1;
+    public const UPLOAD_SAVEAS_FILE = 1;
     /**
      * Form is not a clone
      */
-    const FORM_NOT_CLONED = 0;
+    public const FORM_NOT_CLONED = 0;
     /**
      * Form is a clone
      */
-    const FORM_CLONED = 1;
+    public const FORM_CLONED = 1;
     /**
      * no delay XOOPS redirect delay (in seconds)
      */
-    const REDIRECT_DELAY_NONE = 0;
+    public const REDIRECT_DELAY_NONE = 0;
     /**
      * short XOOPS redirect delay (in seconds)
      */
-    const REDIRECT_DELAY_SHORT = 1;
+    public const REDIRECT_DELAY_SHORT = 1;
     /**
      * medium XOOPS redirect delay (in seconds)
      */
-    const REDIRECT_DELAY_MEDIUM = 3;
+    public const REDIRECT_DELAY_MEDIUM = 3;
     /**
      * long XOOPS redirect delay (in seconds)
      */
-    const REDIRECT_DELAY_LONG = 7;
+    public const REDIRECT_DELAY_LONG = 7;
     /**
      * use captcha settings inherited from XOOPS
      */
-    const CAPTCHA_INHERIT = 0;
+    public const CAPTCHA_INHERIT = 0;
     /**
      * use captcha for ANON users only
      */
-    const CAPTCHA_ANON_ONLY = 1;
+    public const CAPTCHA_ANON_ONLY = 1;
     /**
      * use captcha for everyone
      */
-    const CAPTCHA_EVERYONE = 2;
+    public const CAPTCHA_EVERYONE = 2;
     /**
      * don't use captcha
      */
-    const CAPTCHA_NONE = 3;
+    public const CAPTCHA_NONE = 3;
     /**
      * linefeed delimiter
      */
-    const DELIMITER_BR = 'b';
+    public const DELIMITER_BR = 'b';
     /**
      * white space delimiter
      */
-    const DELIMITER_SPACE = 's';
+    public const DELIMITER_SPACE = 's';
     /**
      * confirm not ok to take action
      */
-    const CONFIRM_NOT_OK = 0;
+    public const CONFIRM_NOT_OK = 0;
     /**
      * confirm ok to take action
      */
-    const CONFIRM_OK = 1;
+    public const CONFIRM_OK = 1;
     /**#@-*/
 }
