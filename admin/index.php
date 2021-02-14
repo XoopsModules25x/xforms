38a04620626a875f06854b2f333e1ca579730dd3
mambax7 <mambax7@gmail.com>
2021-02-14 10:54:50 -0500

    TestdataButtons.php

--- admin/index.php
+++ admin/index.php
@@ -22,18 +22,29 @@
  *
  */
 
-use XoopsModules\Xforms;
-use XoopsModules\Xforms\Constants;
-use XoopsModules\Xforms\Common;
+use Xmf\Module\Admin;
+use Xmf\Request;
+use Xmf\Yaml;
+use XoopsModules\Xforms\{Common,
+    Common\TestdataButtons,
+    Constants,
+    Forms,
+    Helper,
+    Utility
+};
+
+/** @var Admin $adminObject */
+/** @var Helper $helper */
+/** @var Utility $utility */
 
 require_once __DIR__ . '/admin_header.php';
 
 /**
- * @var \Xmf\Module\Admin $adminObject
- * @var \XoopsModules\Xforms\Helper $helper
+ * @var \Xmf\Module\Admin                 $adminObject
+ * @var \XoopsModules\Xforms\Helper       $helper
  * @var \XoopsModules\Xforms\FormsHandler $formsHandler
  *
- * @var string $moduleDirName
+ * @var string                            $moduleDirName
  */
 
 xoops_cp_header();
@@ -50,7 +61,7 @@ $adminObject->addInfoBoxLine(sprintf('<span class="infolabel">' . _MD_XFORMS_TOT
 $adminObject->addInfoBoxLine(sprintf('<span class="infolabel">' . _MD_XFORMS_TOTAL_FORMS . '</span>', '<span class="infotext bold">' . $totalForms . '</span>'));
 
 // Check for profile module
-$profileHelper = \Xmf\Module\Helper::getHelper('profile');
+$profileHelper = Helper::getHelper('profile');
 if (false === $profileHelper) {
     $adminObject->addConfigWarning(sprintf(_MD_XFORMS_PROFILE_NOT_FOUND, $moduleDirName));
 } else {
@@ -58,7 +69,7 @@ if (false === $profileHelper) {
 }
 
 /** @var \XoopsModules\Xforms\Utility $utility */
-$utility = new \XoopsModules\Xforms\Utility();
+$utility = new Utility();
 
 //check for latest release
 //$newRelease = $utility::checkVerModule($helper);
@@ -67,73 +78,24 @@ $utility = new \XoopsModules\Xforms\Utility();
 //}
 
 $adminObject->displayNavigation(basename(__FILE__));
-//------------- Test Data ----------------------------
-
+//------------- Test Data Buttons ----------------------------
 if ($helper->getConfig('displaySampleButton')) {
-    $yamlFile            = dirname(__DIR__) . '/config/admin.yml';
-    $config              = loadAdminConfig($yamlFile);
-    $displaySampleButton = $config['displaySampleButton'];
-
-    if (1 == $displaySampleButton) {
-        xoops_loadLanguage('admin/modulesadmin', 'system');
-        require_once dirname(__DIR__) . '/testdata/index.php';
-
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'ADD_SAMPLEDATA'), '__DIR__ . /../../testdata/index.php?op=load', 'add');
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'SAVE_SAMPLEDATA'), '__DIR__ . /../../testdata/index.php?op=save', 'add');
-        //    $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'EXPORT_SCHEMA'), '__DIR__ . /../../testdata/index.php?op=exportschema', 'add');
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'HIDE_SAMPLEDATA_BUTTONS'), '?op=hide_buttons', 'delete');
-    } else {
-        $adminObject->addItemButton(constant('CO_' . $moduleDirNameUpper . '_' . 'SHOW_SAMPLEDATA_BUTTONS'), '?op=show_buttons', 'add');
-        $displaySampleButton = $config['displaySampleButton'];
-    }
-    $adminObject->displayButton('left', '');
+    TestdataButtons::loadButtonConfig($adminObject);
+    $adminObject->displayButton('left', '');;
 }
-
-//------------- End Test Data ----------------------------
-
-$adminObject->displayIndex();
-
-/**
- * @param $yamlFile
- * @return array|bool
- */
-function loadAdminConfig($yamlFile)
-{
-    $config = \Xmf\Yaml::readWrapped($yamlFile); // work with phpmyadmin YAML dumps
-    return $config;
-}
-
-/**
- * @param $yamlFile
- */
-function hideButtons($yamlFile)
-{
-    $app['displaySampleButton'] = 0;
-    \Xmf\Yaml::save($app, $yamlFile);
-    redirect_header('index.php', 0, '');
-}
-
-/**
- * @param $yamlFile
- */
-function showButtons($yamlFile)
-{
-    $app['displaySampleButton'] = 1;
-    \Xmf\Yaml::save($app, $yamlFile);
-    redirect_header('index.php', 0, '');
-}
-
 $op = \Xmf\Request::getString('op', 0, 'GET');
-
 switch ($op) {
     case 'hide_buttons':
-        hideButtons($yamlFile);
+        TestdataButtons::hideButtons();
         break;
     case 'show_buttons':
-        showButtons($yamlFile);
+        TestdataButtons::showButtons();
         break;
 }
+//------------- End Test Data Buttons ----------------------------
 
+$adminObject->displayIndex();
 echo $utility::getServerStats();
 
+//codeDump(__FILE__);
 require __DIR__ . '/admin_footer.php';
