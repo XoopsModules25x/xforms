e6ec54e4171cfeb8796c25230fd98bd423190258
mambax7 <mambax7@gmail.com>
2021-08-09 14:17:16 -0400

    changelog.txt

--- xoops_version.php
+++ xoops_version.php
@@ -36,7 +36,7 @@ $moduleDirNameUpper = mb_strtoupper($moduleDirName);
 /*  @var array $modversion */
 $modversion['version']       = '2.00';
 $modversion['module_status']  = 'Beta 1';
-$modversion['release_date']   = '2021/02/15';
+$modversion['release_date']   = '2021/08/09';
 $modversion['name']          = _MI_XFORMS_NAME;
 $modversion['description']   = _MI_XFORMS_DESC;
 $modversion['author']        = 'Brandycoke Productions, Dylian Melgert, Juan Garcés';
